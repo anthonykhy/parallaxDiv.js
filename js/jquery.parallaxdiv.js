75d034ac4335a45a483749cc1b0b681172869252
Anthony Kwok <anthonykhy@gmail.com>
2014-02-19 00:43:55 +0800

    Add files
    First version

---
+++ js/jquery.parallaxdiv.js
@@ -0,0 +1,47 @@
+/*Copyright (c) 2014, Anthony Kwok http://at.mosphe.re
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ */
+ 
+(function($) {  
+	$.fn.parallaxDiv = function(PhotoURL,options) {
+		var settings = $.extend({
+			pixelMargin: 20,
+			wrappername: "parallaxWrap"
+		}, options );
+		var element = $(this);  
+		var oriWidth = element.width();
+		var oriHeight = element.height();
+		
+		element.css({
+			"background-repeat": "no-repeat","-webkit-background-size": "cover","-moz-background-size": "cover",
+			"-o-background-size": "cover",
+			"background-size": "cover","background-position": "center center",
+			"background-image":'url("' + PhotoURL + '")',
+			"height":element.height()+(settings.pixelMargin*2),
+			"width":element.width()+(settings.pixelMargin*2),
+		});
+		
+		element.wrapAll('<div id="'+settings.wrappername+'" style="overflow:hidden;padding:0px;margin:0px; width:'+oriWidth+'px; height:'+oriHeight+'px;" ></div>');
+		//for mobile
+		if (window.DeviceMotionEvent != undefined) {
+			window.ondevicemotion = function(e) {
+				//console.log('translate3d('+((e.accelerationIncludingGravity.x/10)*settings.pixelMargin)+'px, '+(-(e.accelerationIncludingGravity.y/10)*settings.pixelMargin)+'px, 0px)');
+				element.css('-webkit-transform', 'translate3d('+(((e.accelerationIncludingGravity.x/10)*settings.pixelMargin-settings.pixelMargin))+'px, '+(((e.accelerationIncludingGravity.y/10)*settings.pixelMargin)-settings.pixelMargin)+'px, 0px)');	
+			}
+		}
+		
+		element.bind("mousemove",function(e){
+			var offset = element.offset(); 
+			var x = e.pageX - offset.left;
+			var y = e.pageY - offset.top;
+			var width = $(this).width();
+			var height = $(this).height();
+			var halfX = width/2;
+			var halfY = height/2;
+			var offsetX = (halfX-x)/halfX;
+			var offsetY = (halfY-y)/halfY;
+
+			element.css('-webkit-transform', 'translate3d('+(offsetX*settings.pixelMargin-settings.pixelMargin)+'px, '+(offsetY*settings.pixelMargin-settings.pixelMargin)+'px, 0px)');
+		});
+	}		  
+})(jQuery);
